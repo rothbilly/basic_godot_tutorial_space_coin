8c69de789e4a6314cb3b7ec57873fe75333aa3e3
rothbilly <roth81ly@gmail.com>
2024-06-13 22:05:49 -0400

    add some stuff to player

--- godot_proyect_space_coin/scripts/Player.gd
+++ godot_proyect_space_coin/scripts/Player.gd
@@ -6,20 +6,23 @@ const VIEW_SCALE = 100
 @export var acelearation :int = 200 
 
 var direction: Vector2 = Vector2.ZERO
+var cust_rot_angle: = 0
 
 @onready var screen_size = get_viewport_rect().size
 @onready var shape = $CollisionShape2D.shape
-	
+
+@onready var tween: Tween = self.create_tween() 
+
 func _physics_process(delta):
 	get_input()
 	
 	velocity = direction * acelearation * delta * VIEW_SCALE
 	set_velocity(velocity)
 	move_and_slide()
-	velocity = velocity
 	
 	position.x = wrapf(position.x, 0, screen_size.x + shape.height * 0.5)
 	position.y = wrapf(position.y, 0, screen_size.y + shape.radius * 0.5)
+	rotation = lerp_angle(rotation, cust_rot_angle, 10 * delta)
 
 func get_input():
 	direction = Vector2.ZERO
@@ -27,6 +30,13 @@ func get_input():
 	direction.x = Input.get_action_strength("ui_right")
 	direction.x = direction.x - Input.get_action_strength("ui_left")
 	
+	if direction.x > 0:
+		cust_rot_angle = 20
+	elif direction.x < 0:
+		cust_rot_angle = -20
+	else:
+		cust_rot_angle = 0
+
 	direction.y = Input.get_action_strength("ui_down")
 	direction.y = direction.y - Input.get_action_strength("ui_up")
 	
@@ -35,4 +45,3 @@ func get_input():
 signal coin_grabed
 func grab_point(_points):
 	emit_signal("coin_grabed", _points)
-
